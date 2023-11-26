36e43e25b41751d6e9ac62b7fc9432744a5e720e
Artem Nistuley <artem.nistuley@gmail.com>
2023-11-26 13:01:56 +0200

    add element with styles/classes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import SimpleGreeting from './lit/simple-greeting';
 import ComposingTemplates from './lit/composing-templates';
 import ToggleElement from './lit/toggle-element';
+import StylesClasses from './lit/styles-classes';
 
 window.customElements.define('simple-greeting', SimpleGreeting);
 window.customElements.define('composing-templates', ComposingTemplates);
 window.customElements.define('toggle-element', ToggleElement);
+window.customElements.define('styles-classes', StylesClasses);
